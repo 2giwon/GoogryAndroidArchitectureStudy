82addeff2fb5518634f24f7e25eb136369ff98b7
middlefgr <2giwon@gmail.com>
2019-12-10 21:13:30 +0900

    # TypeConvertor 코드 수정

--- 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/data/source/local/model/ContentTypeConvertor.kt
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/data/source/local/model/ContentTypeConvertor.kt
@@ -19,7 +19,7 @@ class ContentTypeConvertor {
     }
 
     @TypeConverter
-    fun toString(movies: List<ContentItem>): String {
-        return gson.toJson(movies)
+    fun toString(contentItems: List<ContentItem>): String {
+        return gson.toJson(contentItems)
     }
 }
