426ef42fa6458688cce25e4c143d403df52cb052
middlefgr <2giwon@gmail.com>
2019-12-19 00:10:57 +0900

    # Repository 수정  - 페이징 리스트 추가 하도록 수정  - 리모트 데이터를 가져오는 메소드를 수정    (리모트에서 가져옴 -> 로컬에 저장 -> 로컬에 있는 값을 프레젠터로 전달)    (중간에 가져와서 다른 작업을 할 때 flatmap으로 처리하도록 수정)  - 검색을 다시 할 때 현재까지 검색했던 쿼리에 대한 데이터들을 전부 삭제하도록    쿼리 값을 liveData로 저장하여 새로 검색 시 이용하여 삭제하도록 수정

--- 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/data/NaverDataRepositoryImpl.kt
+++ 2giwon/ArchitectureStudy/app/src/main/java/com/egiwon/architecturestudy/data/NaverDataRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.egiwon.architecturestudy.data
 
+import androidx.lifecycle.MutableLiveData
 import com.egiwon.architecturestudy.data.source.NaverDataSource
 import com.egiwon.architecturestudy.data.source.remote.response.ContentResponse
 import io.reactivex.Single
@@ -11,6 +12,11 @@ class NaverDataRepositoryImpl(
 ) : NaverDataRepository {
 
     private var lastQueryPage = START_PAGE
+
+    private val lastSearchTime = MutableLiveData<Long>()
+
+    private val lastQuery = MutableLiveData<String>()
+
     private fun loadRemoteContents(type: String, query: String): Single<ContentResponse> =
         naverRemoteDataSource.getContents(
             type = type,
@@ -19,46 +25,57 @@ class NaverDataRepositoryImpl(
             start = lastQueryPage
         ).flatMap { response ->
             if (response.contentItems.isNotEmpty()) {
-                lastQueryPage += DISPLAY_PAGE
-                naverLocalDataSource.saveContents(
-                    type,
-                    query,
-                    response
-                ).subscribeOn(Schedulers.io())
-                    .toSingle { response }
+                val immutableLastInsertTime = getLastInsertTime()
+                if (immutableLastInsertTime != null) {
+                    naverLocalDataSource.saveContents(
+                        type,
+                        query,
+                        response,
+                        immutableLastInsertTime
+                    ).andThen(getContents(type, query))
+                } else {
+                    Single.create { Throwable() }
+                }
             } else {
                 Single.create { Throwable() }
             }
-        }
-
+        }.subscribeOn(Schedulers.io())
+            .doOnSubscribe { lastQueryPage += DISPLAY_PAGE }
 
     override fun loadContents(type: String, query: String): Single<ContentResponse> {
         lastQueryPage = START_PAGE
-        return loadRemoteContents(type, query)
+        lastSearchTime.postValue(System.currentTimeMillis())
+        return if (getLastQuery() != null) {
+            naverLocalDataSource.deleteContents(type, getLastQuery()!!)
+                .andThen(loadRemoteContents(type, query))
+        } else {
+            loadRemoteContents(type, query)
+        }.doAfterSuccess { lastQuery.postValue(query) }
     }
 
     override fun getCache(type: String): Single<ContentResponse> =
-        naverLocalDataSource.getCacheContents(type)
-            .doAfterSuccess { lastQueryPage += it.contentItems.size }
+        naverLocalDataSource.getSearchTime(type)
+            .flatMap { insertTime ->
+                if (insertTime != 0L) {
+                    lastSearchTime.postValue(insertTime)
+
+                    naverLocalDataSource.getCacheContents(type)
+                        .doAfterSuccess { lastQueryPage += it.contentItems.size }
+                } else {
+                    Single.create<ContentResponse> { ContentResponse("", emptyList()) }
+                }
+            }
 
 
     override fun requestMore(type: String, query: String): Single<ContentResponse> =
         loadRemoteContents(type, query)
-            .subscribeOn(Schedulers.io())
-            .flatMap {
-                if (it.contentItems.isNotEmpty()) {
-                    getContents(
-                        type,
-                        query
-                    )
-                } else {
-                    Single.create { Throwable() }
-                }
-            }
 
     override fun getContents(type: String, query: String): Single<ContentResponse> =
         naverLocalDataSource.getContents(type, query)
 
+    private fun getLastInsertTime(): Long? = lastSearchTime.value
+
+    private fun getLastQuery(): String? = lastQuery.value
 
     companion object {
         private const val DISPLAY_PAGE = 10
