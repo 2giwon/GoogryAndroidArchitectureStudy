7d0e4a455d01512c9597bc839e15c0dc3b4b0fc2
2giwon <2giwon@gmail.com>
2020-12-05 23:28:39 +0900

    [TEST] - remote Response 데이터 Mock 데이터로 테스트 코드 추가

---
+++ 2giwon/ArchitectureStudy/remote/src/test/java/com/egiwon/remote/entity/Mock.kt
@@ -0,0 +1,331 @@
+package com.egiwon.remote.entity
+
+object Mock {
+    const val MOCK_MOVIE_DATA =
+        """
+        {
+            "title": "극장판 바이올렛 에버가든",
+            "link": "https://movie.naver.com/movie/bi/mi/basic.nhn?code=196843",
+            "image": "https://ssl.pstatic.net/imgmovie/mdi/mit110/1968/196843_P08_111016.jpg",
+            "subtitle": "Violet Evergarden: The <b>Movie</b>",
+            "pubDate": "2020",
+            "director": "이시다테 타이치|",
+            "actor": "이시카와 유이|나미카와 다이스케|",
+            "userRating": "9.50"
+        }
+        """
+
+    const val MOCK_BLOG_DATA =
+        """
+        {
+            "title": "<b>교대역</b> 술집 추천, 도톤보리에서 <b>연말 모임</b>을!",
+            "link": "https://blog.naver.com/tmfrl2865?Redirect=Log&logNo=221397204173",
+            "description": "#교대역술집추천 #교대역이자카야 #교대회식장소 #교대<b>연말모임 교대역</b> 도톤보리의 영업시간 매일 17:00... 장소로도 좋고 연말 모임으로도 좋은 교대 도톤보리 리뷰 끝! + 상당 부분 사비로 먹었는데 정말 맛있었음!",
+            "bloggername": "라온의 순간, 그리고 기록",
+            "bloggerlink": "https://blog.naver.com/tmfrl2865",
+            "postdate": "20181112"
+        }
+        """
+
+    const val MOCK_BOOK_DATA =
+        """
+        {
+            "title": "위대한 계약: 파주, 책, 도시",
+            "link": "https://movie.naver.com/movie/bi/mi/basic.nhn?code=197065",
+            "image": "https://ssl.pstatic.net/imgmovie/mdi/mit110/1970/197065_P01_142213.jpg",
+            "subtitle": "Great Contract: Paju, <b>Book</b>, City",
+            "pubDate": "2020",
+            "director": "정다운|김종신|",
+            "actor": "",
+            "userRating": "0.00"
+        }
+        """
+
+    const val MOCK_NEWS_RESPONSE_DATA =
+        """
+        {
+            "lastBuildDate": "Sat, 05 Dec 2020 22:49:54 +0900",
+            "total": 159679,
+            "start": 1,
+            "display": 10,
+            "items": [
+                {
+                    "title": "장애예술인 활동 동영상 “670만회 기록 대박”",
+                    "originallink": "http://news.kmib.co.kr/article/view.asp?arcid=0015291095&code=61121111&cp=nv",
+                    "link": "https://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=005&aid=0001387838",
+                    "description": "또 심청이의 한을 가진 소리꾼 최예나(시각장애)와 크리에이터 빅<b>마블</b>, 침묵 속의 발레리나 고아라(청각장애)와 마림바 연주자 유니, 세계 유일한 핸디래퍼 김지연(청각장애)과 래퍼 베이식, 웹드라마작가 김뜰... ",
+                    "pubDate": "Sat, 05 Dec 2020 20:28:00 +0900"
+                },
+                {
+                    "title": "2020 인천 국제 1인 미디어 페스티벌 개막…6일까지 진행",
+                    "originallink": "http://www.obsnews.co.kr/news/articleView.html?idxno=1243666",
+                    "link": "http://www.obsnews.co.kr/news/articleView.html?idxno=1243666",
+                    "description": "'더 새로운 연결, 언택트를 넘어 온택트'라는 주제로 개최되는 이번 행사는 코로나19 확산세를 고려해 현장 입장은 통제되며 행사 첫날에는 '빅<b>마블</b>'과 요트원정대 김승진 선장 등이 라이브쇼 등을 진행합니다. 내일은... ",
+                    "pubDate": "Sat, 05 Dec 2020 20:12:00 +0900"
+                },
+                {
+                    "title": "투썸플레이스, 크리스마스 케이크 '윈터 판타지'로 9종 선보여",
+                    "originallink": "http://moneys.mt.co.kr/news/mwView.php?no=2020112912428065369",
+                    "link": "https://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=101&oid=417&aid=0000628071",
+                    "description": "▲블랙베리 샤를로뜨는 윈터 판타지의 신비로움을 퍼플 컬러의 <b>마블</b> 글레이즈로 표현한 케이크다. 유럽 전통 디저트인 샤를로트를 투썸플레이스만의 스타일로 재해석해 초코 가나슈, 베리 쥬레, 마스카포네 생크림을... ",
+                    "pubDate": "Sat, 05 Dec 2020 18:45:00 +0900"
+                },
+                {
+                    "title": "‘블랙팬서’ 레티티아 라이트, 코로나19 백신 음모론에 역풍[할리우드비하인...",
+                    "originallink": "https://www.newsen.com/news_view.php?uid=202012051508342410",
+                    "link": "https://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=106&oid=609&aid=0000367259",
+                    "description": "라이트가 음모론에 동참했고 네티즌들은 그가 혼란을 가중시키고 있다고 질타했다. 한편 레티티아 라이트는 <b>마블</b>영화 '블랙팬서'에서 블랙팬서(채드윅 보스만) 동생 슈리 역으로 얼굴을 알렸다. 사진=ⓒ GettyImagesKorea",
+                    "pubDate": "Sat, 05 Dec 2020 15:09:00 +0900"
+                },
+                {
+                    "title": "2020 인천 국제 1인 미디어 페스티벌 개막",
+                    "originallink": "http://www.obsnews.co.kr/news/articleView.html?idxno=1243620",
+                    "link": "http://www.obsnews.co.kr/news/articleView.html?idxno=1243620",
+                    "description": "'더 새로운 연결, 언택트를 넘어 온택트'라는 주제로 개최되는 이번 행사는 코로나19 확산세를 고려해 현장 입장은 통제되며 행사 첫날에는 '빅<b>마블</b>'과 요트원정대 김승진 선장 등이 라이브쇼 등을 진행합니다. 내일은... ",
+                    "pubDate": "Sat, 05 Dec 2020 14:53:00 +0900"
+                },
+                {
+                    "title": "천안가구단지 성정가구백화점, 12월 맞이 할인 행사 &quot;전시 제품 최대 70%&quot;",
+                    "originallink": "http://www.woodkorea.co.kr/news/articleView.html?idxno=49869",
+                    "link": "http://www.woodkorea.co.kr/news/articleView.html?idxno=49869",
+                    "description": "성정가구백화점은 템퍼, 르<b>마블</b>천안점, 미지트 등 브랜드의 가구를 한눈에 만나볼 수 있는 규모로, 소파나 식탁, 침대, 거실장, 붙박이장 등 다양한 제품을 한 곳에서 비교하고 구매할 수 있다는 특징을 지닌다. 특히... ",
+                    "pubDate": "Sat, 05 Dec 2020 11:30:00 +0900"
+                },
+                {
+                    "title": "레고그룹 ‘레고 게임’ 25주년 기념 캠페인…팟캐스트 시리즈 진행",
+                    "originallink": "http://www.kukinews.com/newsView/kuk202012040304",
+                    "link": "http://www.kukinews.com/newsView/kuk202012040304",
+                    "description": "레고그룹에 따르면 지난 25년간 가장 많이 판매된 레고 게임은 ‘레고 <b>마블</b> 슈퍼 히어로즈’, ‘레고 해리포터: 1~4학년’, ‘레고 스타워즈: 컴플리트 사가’, ‘레고 배트맨: 더 비디오 게임’, ‘레고 배트맨2: DC... ",
+                    "pubDate": "Sat, 05 Dec 2020 08:03:00 +0900"
+                },
+                {
+                    "title": "<b>마블</b> 드라마 &lt;호크아이&gt;, 블랙위도우 세계관 공유하나",
+                    "originallink": "http://www.cine21.com/news/view/?mag_id=96676&utm_source=naver&utm_medium=news",
+                    "link": "https://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=106&oid=140&aid=0000043434",
+                    "description": "디즈니의 스트리밍 서비스 '디즈니 플러스'에서 공개될 <b>마블</b> 드라마 &lt;호크아이&gt;에 플로렌스 퓨, 헤일리 스타인펠드 등이 추가 캐스팅됐다. 지난 2019년 4월 제작 확정이 된 &lt;호크아이&gt;는 MCU(Marvel Cinematic Universe)와 연계된... ",
+                    "pubDate": "Sat, 05 Dec 2020 00:16:00 +0900"
+                },
+                {
+                    "title": "[글로벌-엔터 24] <b>마블</b> 신작 실사 드라마 ‘호크 아이’ 주인공에 ‘블랙 위도...",
+                    "originallink": "http://www.g-enews.com/ko-kr/news/article/news_all/2020120500053262499ecba8d8b8_1/article.html",
+                    "link": "http://www.g-enews.com/ko-kr/news/article/news_all/2020120500053262499ecba8d8b8_1/article.html",
+                    "description": "<b>마블</b> 스튜디오가 제작하는 실사 드라마 ‘호크 아이(hawk eye)’에 스칼렛 요한슨 주연의 ‘블랙 위도우’... 동영상 전달 서비스 ‘Disney+(디즈니 플러스)’로 전달 예정인 이 작품은 <b>마블</b> 시네마틱 유니버스(MCU)에서... ",
+                    "pubDate": "Sat, 05 Dec 2020 00:09:00 +0900"
+                },
+                {
+                    "title": "인천TP, ‘인천 1인 미디어 페스티벌’ 행사",
+                    "originallink": "http://www.obsnews.co.kr/news/articleView.html?idxno=1243532",
+                    "link": "http://www.obsnews.co.kr/news/articleView.html?idxno=1243532",
+                    "description": "이번 행사에는 ‘빅<b>마블</b>’, ‘도티’, ‘블라이미’, ‘한국언니’, ‘래춘씨’, ‘구글 조용민 매니저’... ‘빅 크리에이터 쇼’ 진행을 맡은 ‘빅<b>마블</b>’은 ‘온택트 시대, 글로벌 크리에이터로서의 가능성’... ",
+                    "pubDate": "Fri, 04 Dec 2020 20:18:00 +0900"
+                }
+            ]
+        }
+        """
+
+
+    const val MOCK_BLOG_RESPONSE_DATA =
+        """
+        {
+            "lastBuildDate": "Sat, 05 Dec 2020 22:48:19 +0900",
+            "total": 1141740,
+            "start": 1,
+            "display": 10,
+            "items": [
+                {
+                    "title": "선릉네일 호박<b>마블</b>아트 완전 예쁨!",
+                    "link": "https://blog.naver.com/chuyiyoung?Redirect=Log&logNo=222113796177",
+                    "description": "호박<b>마블</b>아트였어요!! 제가 이 아트를 고른 이유는 기본적인 베이스컬러가 가을느낌과 잘 어울리는 단풍느낌이 나기도 했고, 제 평소 스타일과도 잘 맞는다는 생각이 들었기 때문이에요ㅎㅎ 호박<b>마블</b>아트를... ",
+                    "bloggername": "ღ 이영쓰맛집, 일상 그리고 카페",
+                    "bloggerlink": "https://blog.naver.com/chuyiyoung",
+                    "postdate": "20201012"
+                },
+                {
+                    "title": "핸디청소기 X 홈<b>마블</b> 차량 무선청소기",
+                    "link": "https://blog.naver.com/2322033?Redirect=Log&logNo=222160705943",
+                    "description": "핸디청소기 X 홈<b>마블</b> 차량 무선청소기 아이들이 있다거나 반려동물을 키우는 집이면 모두다 공감... ㅎㅎ 홈<b>마블</b> 차량용 무선청소기 H70 (HVC-H70) 아무리 소형 가전이라 하더라도 한번 구입하면 짧게는 5년... ",
+                    "bloggername": "초코빙수의 무한긍정에너지",
+                    "bloggerlink": "https://blog.naver.com/2322033",
+                    "postdate": "20201202"
+                },
+                {
+                    "title": "이탈리안<b>마블</b>로 샴푸하는 것에 대해",
+                    "link": "https://blog.naver.com/effect302?Redirect=Log&logNo=222143903230",
+                    "description": "저는 <b>마블</b>비누로 샴푸를 하는 것에 약간 불편함을 가지고 있었어요. 아니 아직 익숙해지지 않은 거라고... 샴푸와 <b>마블</b>비누로 왔다갔다를 반복했어요. 그런데 이렇게 양쪽을 오간게 제게 극명한 차이를... ",
+                    "bloggername": "the minimalist kitchen",
+                    "bloggerlink": "https://blog.naver.com/effect302",
+                    "postdate": "20201114"
+                },
+                {
+                    "title": "<b>마블</b>퓨처파이트 PC 통해 블루스택 앱플레이어로 시작해봐요.",
+                    "link": "https://blog.naver.com/fun3200?Redirect=Log&logNo=222141532683",
+                    "description": "전세계적으로 인기가 많은 <b>마블</b> 유니버스의 수많은 세계들을 소재로 한 <b>마블</b> 코믹스! 이 <b>마블</b> 코믹스의 주인공들은 <b>마블</b>퓨처파이트라는 게임에서도 만나볼 수 있는데요. <b>마블</b>의 대표적인 영웅인 캡틴 아메리카... ",
+                    "bloggername": "사라한",
+                    "bloggerlink": "https://blog.naver.com/fun3200",
+                    "postdate": "20201111"
+                },
+                {
+                    "title": "2020년 11월 <b>마블</b> 관련 소식",
+                    "link": "https://blog.naver.com/tomstrong?Redirect=Log&logNo=222140133502",
+                    "description": "*새 시리즈 '소드 (S.W.O.R.D.)' '<b>마블</b> 코믹스'는 오는 2020년 12월에 새로운 온고잉 시리즈 '소드'를 론칭할... 사실 '<b>마블</b>' 팬이라면 '쉴드'는 잘 아실 겁니다. 방패라는 뜻을 가지고 있지만 역시 풀어서 쓰면 정부가... ",
+                    "bloggername": "U GO's Visual Museum",
+                    "bloggerlink": "https://blog.naver.com/tomstrong",
+                    "postdate": "20201110"
+                },
+                {
+                    "title": "차량용 무선청소기 언제 어디서나 편리한 홈<b>마블</b>H80",
+                    "link": "https://blog.naver.com/zaja1972?Redirect=Log&logNo=222107461973",
+                    "description": "홈<b>마블</b>H80이에요. 가정용도 가능해서 원룸에서 사는 1인가구에 딱 맞는 제품인 것 같아요. 제품 포장... 홈<b>마블</b> 핸디 무선청소기 H80은 6000 PA의 흡입력을 자랑하는 강력한 모터에 2단계 속도조절이 가능하고... ",
+                    "bloggername": "가을사랑의 행복을 꿈꾸는 곳",
+                    "bloggerlink": "https://blog.naver.com/zaja1972",
+                    "postdate": "20201005"
+                },
+                {
+                    "title": "카카오 파우더를 넣고 만든 <b>마블</b> 사워도우 빵",
+                    "link": "https://blog.naver.com/jiyulife?Redirect=Log&logNo=222157850245",
+                    "description": "보다가 <b>마블</b> 사워도우가 있길래 만들어봤다. 라미네이션 전 단계에서 반죽을 2:3으로 나눠서 2는 물과 카카오 파우더를 섞어서 준비했다. 늘여 접기는 2번만. 더 많이 하면 다 섞여서 <b>마블</b>이 안보인다. 성형을... ",
+                    "bloggername": "요코하마댁",
+                    "bloggerlink": "https://blog.naver.com/jiyulife",
+                    "postdate": "20201129"
+                },
+                {
+                    "title": "<b>마블</b> 레전드 타노스&amp;블랙오더",
+                    "link": "https://blog.naver.com/hyunjin0905?Redirect=Log&logNo=222162066725",
+                    "description": "어제 저녁 퇴근하고 집에 오니 도착해 있던 <b>마블</b>레전드 타노스&amp;블랙오더 요 몇일동안 예약했던 <b>마블</b> 제품들이 배달 와서 행복하네요 큽니다...커요...!! 제품이 훤히 다 보이는 박스 패키지 보이는 구성이... ",
+                    "bloggername": "☞ 낙서공간 ☜",
+                    "bloggerlink": "https://blog.naver.com/hyunjin0905",
+                    "postdate": "20201203"
+                },
+                {
+                    "title": "홈<b>마블</b> 차량 무선청소기 H70 틈새먼지 완벽정복",
+                    "link": "https://blog.naver.com/mjpslove1?Redirect=Log&logNo=222158276500",
+                    "description": "홈<b>마블</b> 차량 무선청소기인데요 USB 포트 충전으로 220V의 콘센트 없이도 차 안에 구비해놓고 충전하며 사용할수도 있고 기존에 알던 무선청소기들과는 디자인도 더 깔밋하고 예뻐서 여심저격까지 한 홈<b>마블</b>... ",
+                    "bloggername": ":)",
+                    "bloggerlink": "https://blog.naver.com/mjpslove1",
+                    "postdate": "20201130"
+                },
+                {
+                    "title": "신규 심비오트 등장한 <b>마블</b> 퓨처파이트 공략 해보자",
+                    "link": "https://blog.naver.com/mina_100?Redirect=Log&logNo=222135233482",
+                    "description": "신규 심비오트 등장한 <b>마블</b> 퓨처파이트 공략 해보자 안녕하세요. 단테입니다. 많은 사람들이 좋아하는 <b>마블</b> 히어로가 다수 등장하는 게임, <b>마블</b> 퓨처파이트를 기억하시나요? 꾸준한 업데이트로 <b>마블</b>... ",
+                    "bloggername": "리뷰어 단테",
+                    "bloggerlink": "https://blog.naver.com/mina_100",
+                    "postdate": "20201104"
+                }
+            ]
+        }
+        """
+
+
+    const val MOCK_MOVIE_RESPONSE_DATA =
+        """
+        {
+            "lastBuildDate": "Sat, 05 Dec 2020 22:46:18 +0900",
+            "total": 31,
+            "start": 1,
+            "display": 10,
+            "items": [
+                {
+                    "title": "캡틴 <b>마블</b>",
+                    "link": "https://movie.naver.com/movie/bi/mi/basic.nhn?code=132623",
+                    "image": "https://ssl.pstatic.net/imgmovie/mdi/mit110/1326/132623_P22_133853.jpg",
+                    "subtitle": "Captain Marvel",
+                    "pubDate": "2019",
+                    "director": "애너 보든|라이언 플렉|",
+                    "actor": "브리 라슨|사무엘 L. 잭슨|벤 멘델슨|주드 로|",
+                    "userRating": "6.75"
+                },
+                {
+                    "title": "<b>마블</b> 디펜더스",
+                    "link": "https://movie.naver.com/movie/bi/mi/basic.nhn?code=154631",
+                    "image": "https://ssl.pstatic.net/imgmovie/mdi/mit110/1546/154631_P01_092140.jpg",
+                    "subtitle": "Marvel's Defenders",
+                    "pubDate": "2017",
+                    "director": "",
+                    "actor": "핀 존스|존 번탈|마이크 콜터|찰리 콕스|크리스틴 리터|",
+                    "userRating": "9.30"
+                },
+                {
+                    "title": "<b>마블</b> 아이언 피스트 시즌1",
+                    "link": "https://movie.naver.com/movie/bi/mi/basic.nhn?code=154182",
+                    "image": "https://ssl.pstatic.net/imgmovie/mdi/mit110/1541/154182_P06_140513.jpg",
+                    "subtitle": "Marvel's Iron Fist",
+                    "pubDate": "2017",
+                    "director": "",
+                    "actor": "핀 존스|제시카 헨윅|데이비드 웬햄|제시카 스트롭|톰 펠프레이|",
+                    "userRating": "5.33"
+                },
+                {
+                    "title": "<b>마블</b> 루크 케이지",
+                    "link": "https://movie.naver.com/movie/bi/mi/basic.nhn?code=138463",
+                    "image": "https://ssl.pstatic.net/imgmovie/mdi/mit110/1384/138463_P07_103237.jpg",
+                    "subtitle": "Marvel's Luke Cage",
+                    "pubDate": "2016",
+                    "director": "",
+                    "actor": "마이크 콜터|마허샬라 알리|알프리 우다드|테오 로시|프랭크 월리|로사리오 도슨|시몬 미식|",
+                    "userRating": "8.50"
+                },
+                {
+                    "title": "<b>마블</b> 데어데블 시즌1",
+                    "link": "https://movie.naver.com/movie/bi/mi/basic.nhn?code=127828",
+                    "image": "https://ssl.pstatic.net/imgmovie/mdi/mit110/1278/127828_P01_150806.jpg",
+                    "subtitle": "Marvel's Daredevil",
+                    "pubDate": "2015",
+                    "director": "",
+                    "actor": "찰리 콕스|엘든 헨슨|빈센트 도노프리오|로사리오 도슨|데보라 앤 월|존 번탈|스콧 글렌|스티븐 라이더|본디 커티스 홀|에로디 영|",
+                    "userRating": "8.07"
+                },
+                {
+                    "title": "<b>마블</b> 제시카 존스 시즌1",
+                    "link": "https://movie.naver.com/movie/bi/mi/basic.nhn?code=136921",
+                    "image": "https://ssl.pstatic.net/imgmovie/mdi/mit110/1369/136921_P01_153922.jpg",
+                    "subtitle": "Marvel's Jessica Jones",
+                    "pubDate": "2015",
+                    "director": "",
+                    "actor": "크리스틴 리터|데이비드 테넌트|마이크 콜터|레이첼 테일러|",
+                    "userRating": "7.60"
+                },
+                {
+                    "title": "<b>마블</b> 원샷",
+                    "link": "https://movie.naver.com/movie/bi/mi/basic.nhn?code=143826",
+                    "image": "https://ssl.pstatic.net/imgmovie/mdi/mit110/1438/143826_P01_135812.png",
+                    "subtitle": "Marvel One-Shots",
+                    "pubDate": "2015",
+                    "director": "",
+                    "actor": "",
+                    "userRating": "5.50"
+                },
+                {
+                    "title": "올웨이즈 와칭: 어 <b>마블</b> 호네츠 스토리",
+                    "link": "https://movie.naver.com/movie/bi/mi/basic.nhn?code=144665",
+                    "image": "https://ssl.pstatic.net/imgmovie/mdi/mit110/1446/144665_P01_182242.jpg",
+                    "subtitle": "Always Watching: A Marble Hornets Story",
+                    "pubDate": "2015",
+                    "director": "제임스 모랜|",
+                    "actor": "제이크 맥더맨|알렉산드라 브렉켄릿지|",
+                    "userRating": "9.00"
+                },
+                {
+                    "title": "<b>마블</b>원샷: 올 헤일 더 킹",
+                    "link": "https://movie.naver.com/movie/bi/mi/basic.nhn?code=121750",
+                    "image": "https://ssl.pstatic.net/imgmovie/mdi/mit110/1217/121750_P01_171643.jpg",
+                    "subtitle": "Marvel One-Shot: All Hail the King",
+                    "pubDate": "2014",
+                    "director": "드류 피어스|",
+                    "actor": "벤 킹슬리|",
+                    "userRating": "7.67"
+                },
+                {
+                    "title": "<b>마블</b>의 75년 : 프롬 펄프 투 팝!",
+                    "link": "https://movie.naver.com/movie/bi/mi/basic.nhn?code=141859",
+                    "image": "https://ssl.pstatic.net/imgmovie/mdi/mit110/1418/141859_P01_142434.jpg",
+                    "subtitle": "Marvel 75 Years: From Pulp to Pop!",
+                    "pubDate": "2014",
+                    "director": "잭 넛슨|",
+                    "actor": "악셀 알론소|헤일리 앳웰|게리 콘웨이|루이스 데스포지토|앨런 파인|세스 그린|클락 그레그|지미 키멜|스탠 리|토드 맥팔레인|패튼 오스왈트|니콜 펄맨|조 쿠에사다|케빈 스미스|짐 스탈린|에밀리 반캠프|렌 와인|밍나 웬|",
+                    "userRating": "10.00"
+                }
+            ]
+        }
+        """
+}
